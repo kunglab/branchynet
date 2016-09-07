b8b575f3886296d9785b00bf182805a2d40dd7f3
Brad McDanel <mcdabw6@gmail.com>
2016-09-07 14:40:47 -0400

    added drop network idea

---
+++ networks/mlp.py
@@ -0,0 +1,20 @@
+import chainer
+import chainer.functions as F
+import chainer.links as L
+from chainer import training
+from chainer.training import extensions
+
+# Network definition
+class MLP(chainer.Chain):
+    def __init__(self, n_in, n_units, n_out):
+        super(MLP, self).__init__(
+            l1=L.Linear(n_in, n_units),  # first layer
+            l2=L.Linear(n_units, n_units),  # second layer
+            l3=L.Linear(n_units, n_out),  # output layer
+        )
+
+    def __call__(self, x):
+        h1 = F.relu(self.l1(x))
+        h2 = F.relu(self.l2(h1))
+        return self.l3(h2)
+    
