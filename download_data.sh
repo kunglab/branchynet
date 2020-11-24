d5d60a373de52c2bcaa1de9bfc42cef6ce2e350b
Surat Teerapittayanon <steerapi@gmail.com>
2020-11-24 09:57:05 +0700

    Update download_data.sh

--- download_data.sh
+++ download_data.sh
@@ -2,4 +2,5 @@
 mkdir -p datasets/data/pcifar10
 cd datasets/data/pcifar10
 # or download from here https://drive.google.com/file/d/0Byyuc5LmNmJPWUc5dVdUSms3U1E/view?usp=sharing
-wget 'https://dl.dropboxusercontent.com/u/4542002/branchynet/data.npz' -O data.npz
+echo "Please download data from https://drive.google.com/file/d/0Byyuc5LmNmJPWUc5dVdUSms3U1E/view?usp=sharing and put it at datasets/data/pcifar10/data.npz"
+# wget 'https://dl.dropboxusercontent.com/u/4542002/branchynet/data.npz' -O data.npz
